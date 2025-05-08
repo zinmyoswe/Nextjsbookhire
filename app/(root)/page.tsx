a8866de9ca7ad633c855444a680510f1e36d01e9
zinmyoswe <zinmyoswe@github.com>
2025-05-09 04:11:29 +0700

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <Button>Click Me</Button>
+      <p className="mx-4 my-4 w-xl rounded-md border border-amber-300 border-2 px-4 py-4 text-center text-amber-400">ABC</p>
+    </>
+  )
+}
+
+export default Home;
