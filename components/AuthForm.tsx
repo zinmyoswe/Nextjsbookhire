d2a676a310f91c7570ded96f20532857f11d6d6e
zinmyoswe <zinmyoswe@github.com>
2025-05-10 04:44:16 +0700

    form input for singin and signup page

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react'
-import { DefaultValues, FieldValue, FieldValues, SubmitHandler, useForm, UseFormReturn } from "react-hook-form"
+import { DefaultValues, FieldValue, FieldValues, Path, SubmitHandler, useForm, UseFormReturn } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z, ZodType } from "zod";
 
@@ -16,6 +16,8 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import Link from 'next/link';
+import { FIELD_NAMES } from '@/constants';
 
 interface Props<T extends FieldValues> {
     schema: ZodType<T>;
@@ -25,7 +27,10 @@ interface Props<T extends FieldValues> {
 }
 
 const AuthForm = <T extends FieldValues>({ type, schema, defaultValues, onSubmit }: Props<T>) => {
-  // 1. Define your form.
+  
+  const isSignIn = type === "SIGN_IN";
+
+    // 1. Define your form.
   const form: UseFormReturn<T> = useForm({
     resolver: zodResolver(schema),
     defaultValues: defaultValues as DefaultValues<T>,
@@ -35,14 +40,29 @@ const AuthForm = <T extends FieldValues>({ type, schema, defaultValues, onSubmit
   const handleSubmit: SubmitHandler<T> = async (data) => {};
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
+    <div className='flex flex-col gap-4'>
+        <h1 className='text-2xl font-semibold text-white'>
+            {isSignIn ? "Welcome back to PeachCherryBook" : "Create your library account"}
+        </h1>
+
+        <p className="text-light-100">
+        {isSignIn
+          ? "Access the vast collection of resources, and stay updated"
+          : "Please complete all fields and upload a valid university ID to gain access to the library"}
+      </p>
+        <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6 w-full">
+
+        {Object.keys(defaultValues).map((field) => (
+            <FormField
+                key={field}
+                control={form.control}
+                name={field as Path<T>}
+                render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel className='capitalize'>
+                {FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}
+              </FormLabel>
               <FormControl>
                 <Input placeholder="shadcn" {...field} />
               </FormControl>
@@ -53,9 +73,28 @@ const AuthForm = <T extends FieldValues>({ type, schema, defaultValues, onSubmit
             </FormItem>
           )}
         />
+        ))}
+        
         <Button type="submit">Submit</Button>
       </form>
     </Form>
+
+    <p className='text-center text-base font-medium'>
+        {isSignIn ? "New to PeachCherryBook? " : "Already have an account? "}
+
+        <Link
+            href={isSignIn ? "/sign-up" : "/sign-in"}
+            className='font-bold text-primary'
+        >
+            {isSignIn ? "Create an account" : "Sign in"}
+        </Link>
+    </p>
+
+    
+
+
+    </div>
+    
   )
 }
 
